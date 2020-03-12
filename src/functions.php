bb292f5e4e75aaec9fee91c33601076511a35bdc
Andrey <alestro@bk.ru>
2020-03-12 18:13:26 +0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -16,3 +16,227 @@ function arguments(array $arguments) : array {
 
     return $arguments;
 }
+
+
+/**
+ * @param array $array
+ * @return bool
+ */
+function is_assoc(array $array) : bool {
+    foreach ($array as $offset => $value){
+        if(is_string($offset)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * @param $var
+ * @return bool
+ */
+function is_accessible($var) : bool {
+    return is_array($var) || $var instanceof \ArrayAccess;
+}
+
+/**
+ * @param array $array
+ * @param $offset
+ * @return mixed|null
+ */
+function array_pull(array &$array, $offset){
+    
+    $v = array_get($array, $offset);
+
+    array_remove($array, $offset);
+
+    return $v;
+}
+
+
+/**
+ * @param array $array
+ * @param $offset
+ * @param null $default
+ * @return mixed|null
+ */
+function array_get(array $array, $offset, $default = null){
+    
+    if(is_string($offset) && is_dot($offset)){
+
+        $segments = dot_explode($offset);
+
+        $offset = array_pop($segments);
+
+        foreach ($segments as $segment){
+            
+            if(array_key_exists($segment, $array) && is_array($array[$segment])){
+                $array = $array[$segment];
+            } 
+            
+            else {
+                return $default;
+            }
+        }
+    }
+
+    return array_key_exists($offset, $array) ? $array[$offset] : $default;
+}
+
+/**
+ * @param array $array
+ * @param $offset
+ * @param $value
+ */
+function array_set(array &$array, $offset, $value) : void {
+
+    if($offset === null){
+        $array[] = $value;
+        return;
+    }
+
+    if(is_string($offset) && is_dot($offset)){
+
+        $segments = dot_explode($offset);
+
+        $offset = array_pop($segments);
+
+        foreach ($segments as $segment){
+            $array[$segment] = [];
+            $array =& $array[$segment];
+        }
+    }
+
+    $array[$offset] = $value;
+}
+
+/**
+ * @param array $array
+ * @param array|string|int $offset
+ * @param string|int ...$offsets
+ * @return array
+ */
+function array_only(array $array, $offset, ...$offsets) : array {
+
+    if(is_array($offset)){
+        $offsets = $offset;
+    } 
+    
+    else {
+        array_unshift($offsets, $offset);
+    }
+
+    $values = [];
+
+    foreach ($offsets as $offset){
+        if(array_has($array, $offset)){
+            $values[$offset] = array_get($array, $offset);
+        }
+    }
+
+    return $values;
+}
+
+/**
+ * @param array $array
+ * @param array|string|int $offset
+ * @param string|int ...$offsets
+ * @return array
+ */
+function array_except(array $array, $offset, ...$offsets) : array {
+
+    if(is_array($offset)){
+        $offsets = $offset;
+    } 
+    
+    else {
+        array_unshift($offsets, $offset);
+    }
+
+    foreach ($offsets as $offset){
+        array_remove($array, $offset);
+    }
+
+    return $array;
+}
+
+/**
+ * @param array $array
+ * @param $offset
+ * @return bool
+ */
+function array_has(array $array, $offset) : bool {
+
+    if(is_string($offset) && is_dot($offset)){
+
+        $segments = dot_explode($offset);
+
+        $offset = array_pop($segments);
+
+        foreach ($segments as $segment){
+            
+            if(array_key_exists($segment, $array) && is_array($array[$segment])){
+                $array = $array[$segment];
+            } 
+            
+            else {
+                return false;
+            }
+        }
+    }
+
+    return array_key_exists($offset, $array);
+}
+
+/**
+ * @param array $array
+ * @param $offset
+ */
+function array_remove(array &$array, $offset) : void {
+
+    if(is_string($offset) && is_dot($offset)){
+
+        $segments = dot_explode($offset);
+
+        $offset = array_pop($segments);
+
+        foreach ($segments as $segment){
+            
+            if(array_key_exists($segment, $array) && is_array($array[$segment])){
+                $array =& $array[$segment];
+            } 
+            
+            else {
+                return;
+            }
+        }
+    }
+
+    unset($array[$offset]);
+}
+
+/**
+ * @param string $haystack
+ * @param string $needle
+ * @return bool
+ */
+function contains(string $haystack, string $needle) : bool {
+    return mb_strpos($haystack, $needle) !== false;
+}
+
+/**
+ * @param string $string
+ * @return bool
+ */
+function is_dot(string $string) : bool {
+    return containts($string, '.');
+}
+
+/**
+ * @param string $string
+ * @return array
+ */
+function dot_explode(string $string) : array {
+    return explode('.', $string);
+}
